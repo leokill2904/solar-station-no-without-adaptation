d400d8e43ef484ab41d7534a5ccebebecf12a844
Leonid Tanasiienko <tanasyenko029@gmail.com>
2023-01-20 16:31:27 +0200

    first
    first

---
+++ js/working.js
@@ -0,0 +1,15 @@
+const items = document.querySelectorAll(".working button");
+
+function toggleWorking() {
+  const itemToggle = this.getAttribute("aria-expanded");
+
+  for (i = 0; i < items.length; i++) {
+    items[i].setAttribute("aria-expanded", "false");
+  }
+
+  if (itemToggle == "false") {
+    this.setAttribute("aria-expanded", "true");
+  }
+}
+
+items.forEach((item) => item.addEventListener("click", toggleWorking));
